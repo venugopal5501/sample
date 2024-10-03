474db58122e2929e9c18b846080b9f16f4e9519f
venugopal5501 <venugopal14082002@gmail.com>
2024-10-03 18:21:00 +0530

    vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,84 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <img alt="images" src="../assets/yes.png">
+  <h1 v-on:mousemove="getConsole()">Home Component length of {{ "Digital" }} is {{ "Digital".length }}</h1>
+  <h1>{{ message }}</h1>
+  <h1>{{ email }}</h1>
+  <h1>{{ mobile }}</h1>
+  <h2>Address : {{ getAddress("Sivakasi") }}</h2>
+  <h3>{{ getData().text }} {{ getData().year }}</h3>
+  <h4>{{ getData().disp }}</h4>
+  <button v-on:click="getNotify()">Click Me</button>
+  <button v-on:click="getNotify('Button 1 Click')">Click</button>
+  <button v-on:dblclick="getDouble('Button 2 Double Click')">Click Here</button>
+  <button v-on:click="getCount()">Count</button>
+  <button v-on:click="getDecrease()">Decrease</button>
+  <h1>{{ count }}</h1>
+  <input type="text" v-model="sam" />
+  <input type="text" v-model="emails"/>
+  <input type="text" v-model="pass">
+  <h6>{{ sam }}</h6>
+  <h5>{{ emails }}</h5>
+  <h4>{{ pass }}</h4>
+  <button v-on:click="getWarn()">Get Details</button>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'venuGopal',
+  data() {
+    return {
+      email: 'venugopal@gmail.com',
+      mobile: 9080368568,
+      count: 0,
+      sam: 0,
+      emails: null,
+      pass: null,
+      getAddress(address) {
+        return address
+      }
+    }
+  },
+  methods: {
+    getData() {
+      return {
+        disp: this.email,
+        text: 'Copyright',
+        year: '@2024'
+      }
+    },
+    getNotify() {
+      alert("Function Called")
+    },
+    getDouble() {
+      alert("Double click function called")
+    },
+    getConsole() {
+      console.log("Event is fired");
+    },
+    getCount() {
+      this.count = this.count + 1;
+    },
+    getDecrease() {
+      this.count = this.count - 1;
+    },
+    getWarn(){
+      alert(this.emails +" "+this.pass);
+    }
+  },
+
+
   props: {
-    msg: String
+    message: String
   }
 }
+
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+<style>
+h1 {
+  color: rgb(38, 1, 250);
 }
 </style>
